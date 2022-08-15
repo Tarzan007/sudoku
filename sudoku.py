672de404279d16474412b5fa2860c3148b9f9188
Tarzan007 <44656001+Tarzan007@users.noreply.github.com>
2022-08-14 20:54:06 -0400

    Adding main frame work to sudoku back end and creating classes

---
+++ sudoku.py
@@ -0,0 +1,53 @@
+from enum import Enum
+import numpy as np
+
+
+class Sudoku:
+    
+    def __init__(self):
+        self.board = []
+        self.curAction = self.Action.ADD
+        pass
+    
+    def onClick(self, table):
+        pass
+        
+    
+    def delete(self):
+        pass
+    
+    def add(self):
+        pass
+    
+    def undo(self):
+        pass
+    
+    class Action(Enum):
+        ADD = 1
+        ERASE = 2
+        NOTES = 3
+        
+     
+    
+    class Table:
+        def __init__(self, tablePos):
+            # Keep track of the position of the table in sudoku
+            self.tablePos = tablePos
+            # Keeping track of each block on the table row by row
+            self.board = []
+            # Filling each row with a block
+            for i in range(len(9)):
+                self.board.append(self.Block(i))
+    
+    
+    class Block:
+        def __init__(self, blockPos):
+            # Keep track of the value in the clock
+            self.value = None
+            # Keeping track of the notes on the block 
+            self.notes = [0 for x in range(len(9))]
+            self.blockPos = blockPos
+            
+            
+if __name__ == '__main__':
+    pass
